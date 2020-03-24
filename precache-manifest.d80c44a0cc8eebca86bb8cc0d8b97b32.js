f3a51ed1b5fdfbc024df41942b34b6f5a9fb1196
mostafafarag <mostafafarag140@gmail.com>
2020-03-25 00:43:17 +0200

    Updates

---
+++ precache-manifest.d80c44a0cc8eebca86bb8cc0d8b97b32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4326bded2c8539639406f8fafc860f7a",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "05e4ee7feac977c8f012",
+    "url": "/my-portfolio/static/css/main.ffb7f637.chunk.css"
+  },
+  {
+    "revision": "7061640bdffddf71079f",
+    "url": "/my-portfolio/static/js/2.7b0d3aaf.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/my-portfolio/static/js/2.7b0d3aaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05e4ee7feac977c8f012",
+    "url": "/my-portfolio/static/js/main.506dcdbe.chunk.js"
+  },
+  {
+    "revision": "d4a6f897414192edb936",
+    "url": "/my-portfolio/static/js/runtime-main.7e7d7162.js"
+  }
+]);
