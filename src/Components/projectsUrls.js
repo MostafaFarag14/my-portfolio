2c26dd51afa5b86ad16dbefc1384ebc6ac0722f5
mostafafarag <mostafafarag140@gmail.com>
2020-03-25 00:41:53 +0200

    first

--- src/Components/projectsUrls.js
+++ src/Components/projectsUrls.js
@@ -3,10 +3,10 @@ export const projectsUrls = [
     name: "Moive-App",
     url: "https://ree-action.netlify.com/",
     code: "https://github.com/MostafaFarag14/Movie-App.git",
-    image: "https://previews.dropbox.com/p/thumb/AAuhERJ9M1_OFj55MvSRcukKjAtYXQJvYxxb6VVx30Wy5e7BMW1P5tOAoMma1gCbtPDP2vTL1mQqr96w9vXG-im_75YXNjxVXzLeJ5IprZOq8RO2z2LVbQ2SjQcmOIi-lv95aiZC-CJ7mI0lt1IXescTTkYlFi4M3ojrE1GAAHYwGYc-AFoDs7z3FdaLoUTkkuCbsjmWfPtYCYMdo6NuY842DnudR6Ue4FSLjccPcZnd0wsIF5rsUYIUYWUsrseLpeYP0xXWxZhdbA6LVN38mx-tA1oXEvhiMiQ_Drc_sYVD6csFJUPflaVp0Bt7qDoPkvqLE4OhJSPdVoD10nS_qzaC/p.png?fv_content=true&size_mode=5",
+    image: "/img/reeaction.PNG",
     tools: [
-      "https://drive.google.com/drive/folders/1D9bZAY8hClNGey6o9SWfhiFp5qgKyQQg/react.png",
-      "https://drive.google.com/drive/folders/1D9bZAY8hClNGey6o9SWfhiFp5qgKyQQg/bootstrap-solid.svg"
+      "/img/react.png",
+      "/img/bootstrap-solid.svg"
     ],
     description: `A movie browser react app that interacts with The Movie DB API to get trending,
      top rated, popular and upcoming movies. also it has a live search for a certain movie. 
@@ -18,10 +18,10 @@ export const projectsUrls = [
     name: "Jammming",
     url: "https://jammming-hits.netlify.com/",
     code: "https://github.com/MostafaFarag14/Jammming-react-app.git",
-    image: "https://drive.google.com/drive/folders/1D9bZAY8hClNGey6o9SWfhiFp5qgKyQQg/jamming.PNG",
+    image: "/img/jamming.PNG",
     tools: [
-      "https://drive.google.com/drive/folders/1D9bZAY8hClNGey6o9SWfhiFp5qgKyQQg/react.png",
-      "https://drive.google.com/drive/folders/1D9bZAY8hClNGey6o9SWfhiFp5qgKyQQg/css.png"
+      "/img/react.png",
+      "/img/css.png"
 
     ],
     description: `A react app that interacts with Spotify API to allow users to search the Spotify library,
@@ -31,11 +31,11 @@ export const projectsUrls = [
     name: "ShooterGame",
     url: "https://mostafafarag14.github.io/shooter-game/",
     code: "https://github.com/MostafaFarag14/shooter-game.git",
-    image: "https://drive.google.com/drive/folders/1D9bZAY8hClNGey6o9SWfhiFp5qgKyQQg/shooter.PNG",
+    image: "/img/shooter.PNG",
     tools: [
-      "https://drive.google.com/drive/folders/1D9bZAY8hClNGey6o9SWfhiFp5qgKyQQg/javascript.png",
-      "https://drive.google.com/drive/folders/1D9bZAY8hClNGey6o9SWfhiFp5qgKyQQg/html.png",
-      "https://drive.google.com/drive/folders/1D9bZAY8hClNGey6o9SWfhiFp5qgKyQQg/css.png"
+      "/img/javascript.png",
+      "/img/html.png",
+      "/img/css.png"
     ],
     description: `It's a shooting game that allows the user to select 3 levels and 3 characters.
     each level has a different environment and spawning frequency of 2 different kinds enemies.
@@ -45,10 +45,10 @@ export const projectsUrls = [
     name: "Calculator",
     url: "https://mostafafarag14.github.io/calculator/",
     code: "https://github.com/MostafaFarag14/calculator.git",
-    image: "https://drive.google.com/drive/folders/1D9bZAY8hClNGey6o9SWfhiFp5qgKyQQg/calculator.PNG",
+    image: "/img/calculator.PNG",
     tools: [
-      "https://drive.google.com/drive/folders/1D9bZAY8hClNGey6o9SWfhiFp5qgKyQQg/react.png",
-      "https://drive.google.com/drive/folders/1D9bZAY8hClNGey6o9SWfhiFp5qgKyQQg/bootstrap-solid.svg"
+      "/img/react.png",
+      "/img/bootstrap-solid.svg"
     ],
     description: `A simple calculator using React`
   }
@@ -57,9 +57,9 @@ export const projectsUrls = [
     name: "PianoPlayer",
     url: "https://mostafafarag14.github.io/Piano-player/",
     code: "https://github.com/MostafaFarag14/Piano-player.git",
-    image: "https://drive.google.com/drive/folders/1D9bZAY8hClNGey6o9SWfhiFp5qgKyQQg/piano.PNG",
+    image: "/img/piano.PNG",
     tools: [
-      "https://drive.google.com/drive/folders/1D9bZAY8hClNGey6o9SWfhiFp5qgKyQQg/javascript.png"
+      "/img/javascript.png"
     ],
     description: `A piano player app which i have added functionality to it using javascript and made it 
     playable using mouse or keyboard keys by mapping each key or mouse click to certain audio`
@@ -68,11 +68,11 @@ export const projectsUrls = [
     name: "ChoreDoor",
     url: "https://mostafafarag14.github.io/Chore-Door/",
     code: "https://github.com/MostafaFarag14/Chore-Door.git",
-    image: "https://drive.google.com/drive/folders/1D9bZAY8hClNGey6o9SWfhiFp5qgKyQQg/choreDoor.PNG",
+    image: "/img/choreDoor.PNG",
     tools: [
-      "https://drive.google.com/drive/folders/1D9bZAY8hClNGey6o9SWfhiFp5qgKyQQg/javascript.png",
-      "https://drive.google.com/drive/folders/1D9bZAY8hClNGey6o9SWfhiFp5qgKyQQg/html.png",
-      "https://drive.google.com/drive/folders/1D9bZAY8hClNGey6o9SWfhiFp5qgKyQQg/css.png"
+      "/img/javascript.png",
+      "/img/html.png",
+      "/img/css.png"
     ],
     description: `A game using html and javascript. There is a Bot hiding behind one of 3 doors,
     Your mission is to open two Consecutive doors without running into the ChoreBot to win.
@@ -81,40 +81,40 @@ export const projectsUrls = [
   {
     name: "Memory Allocation",
     code: "https://github.com/MostafaFarag14/Memory-allocation.git",
-    image: "https://drive.google.com/drive/folders/1D9bZAY8hClNGey6o9SWfhiFp5qgKyQQg/allocation.png",
+    image: "/img/allocation.png",
     description: `A Desktop App built using c# and winForms to simulate allocating of proccesses algorithms`,
     tools: [
-      "https://drive.google.com/drive/folders/1D9bZAY8hClNGey6o9SWfhiFp5qgKyQQg/cSharp.png",
-      "https://drive.google.com/drive/folders/1D9bZAY8hClNGey6o9SWfhiFp5qgKyQQg/winform.png"
+      "/img/cSharp.png",
+      "/img/winform.png"
     ]
   },
   {
     name: "DES Function",
     code: "https://github.com/MostafaFarag14/DES-Function.git",
-    image: "https://drive.google.com/drive/folders/1D9bZAY8hClNGey6o9SWfhiFp5qgKyQQg/round_function.jpg",
+    image: "/img/round_function.jpg",
     description: `A Console App with C++ to implement DES Function`,
     tools: [
-      "https://drive.google.com/drive/folders/1D9bZAY8hClNGey6o9SWfhiFp5qgKyQQg/cplpl.png",
+      "/img/cplpl.png",
     ]
   },
   {
     name: "Scanner And Parser",
     code: "https://github.com/MostafaFarag14/Parser.git",
-    image: "https://drive.google.com/drive/folders/1D9bZAY8hClNGey6o9SWfhiFp5qgKyQQg/parser.jpg",
+    image: "/img/parser.jpg",
     description: `Scanner read the input from a file containing sample TINY code,
     then identifies the tokens and passes that list of tokens to the parser to get 
     Recognized structures and saves it to a text file` ,
     tools: [
-      "https://drive.google.com/drive/folders/1D9bZAY8hClNGey6o9SWfhiFp5qgKyQQg/cplpl.png",
+      "/img/cplpl.png",
     ]
   },
   {
     name: "Job scheduler",
     code: "https://github.com/MostafaFarag14/Job-scheduler.git",
-    image: "https://drive.google.com/drive/folders/1D9bZAY8hClNGey6o9SWfhiFp5qgKyQQg/Job scheduler.png",
+    image: "/img/Job scheduler.png",
     description: `A Console c++ app implements scheduling algorithms like FCFS, higher priority first , SJF and round robin`,
     tools: [
-      "https://drive.google.com/drive/folders/1D9bZAY8hClNGey6o9SWfhiFp5qgKyQQg/cplpl.png",
+      "/img/cplpl.png",
     ]
 
   }
